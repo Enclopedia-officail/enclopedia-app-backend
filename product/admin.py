20095fb6033fcb2d8995d84d2b8cdf098e241eae
naokiyamaguchi <naokiyamaguchi@gmail.com>
2022-12-31 16:01:33 +0900

    modified Size admin display of product model

--- product/admin.py
+++ product/admin.py
@@ -27,11 +27,15 @@ class ShippingAdmin(admin.ModelAdmin):
 class TagAdmin(admin.ModelAdmin):
     list_display = ('id', 'tag_name')
 
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ('product', 'product_product_name', 'size')
+    search_fields = ['product__id', 'product__product_name']
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation, VariationAdmin)
 admin.site.register(ReviewRating, ReviewRatingAdmin)
 admin.site.register(ImageGallary, ImageGallaryAdmin)
-admin.site.register(Size)
+admin.site.register(Size, SizeAdmin)
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Shipping, ShippingAdmin)
 admin.site.register(Price, PriceAdmin)
