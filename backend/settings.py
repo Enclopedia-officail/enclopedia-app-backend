ba61106046119733c11e2cf0bb94dbaa3610ee70
naokiyamaguchi <naokiyamaguchi@gmail.com>
2023-02-19 18:54:15 +0900

    modified settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -16,9 +16,9 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['api.enclopedia-official.com', 'www.enclopedia-official.com']
 CSRF_TRUSTED_ORIGINS = ['https://api.enclopedia-official.com']
 
 # STRIPE API KEY
