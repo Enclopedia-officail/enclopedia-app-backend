4178df5f43bc10632c724cd5a3b8fd9ca5417518
naokiyamaguchi <naokiyamaguchi@gmail.com>
2023-02-26 17:04:05 +0900

    modified buying task

--- buying/signals.py
+++ buying/signals.py
@@ -1,17 +1,23 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from .tasks import order_completed_notification, order_canceled_notification
-from .models import Order
+from .models import Order, OrderItem
 import logging
 
 logger = logging.getLogger(__name__)
 
+
+@receiver(post_save, sender=OrderItem)
+def create_order_item(sender, instance, created, **kwargs):
+    if created and instance.is_ordered:
+        order_completed_notification(instance)
+    else:
+        pass
+
 @receiver(pre_save, sender=Order)
 def updated_order(sender, instance, update_fields, *args, **kwargs):
     if ("status" in list(update_fields)):
-        if instance.status == 'completed':
-            order_completed_notification(instance)
-        elif instance.status == 'cancelled':
+        if instance.status == 'cancelled':
             order_canceled_notification(instance)
         else:
             logger.info('orderモデルのupdate_fieldsが存在しません。')
