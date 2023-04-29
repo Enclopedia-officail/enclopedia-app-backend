e8266351b3233aa910067bd6861b98ea71ceaf6e
naokiyamaguchi <naokiyamaguchi@gmail.com>
2023-04-29 23:31:55 +0900

    modified chatgpt function

--- chatgpt/models.py
+++ chatgpt/models.py
@@ -6,9 +6,15 @@ from user.models import Account
 class Styling(models.Model):
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True, unique=True)
+    age = models.IntegerField()
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    size = models.CharField(max_length=30)
     silhouette = models.CharField(max_length=100)
+    #好きなブランド
     brand = models.CharField(max_length=100)
     situation = models.CharField(max_length=100)
+    style = models.CharField(max_length=100)
     color = models.CharField(max_length=100)
     material = models.CharField(max_length=100)
 
--- chatgpt/views.py
+++ chatgpt/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
-
+from rest_framework.permissions import AllowAny
 # Create your views here.
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from rest_framework.permissions import AllowAny
 from user.models import Profile
 
 import environ
+import datetime
 import openai
+import deepl
+import json
 
 env = environ.Env()
+DEEPL_SECRET_KEY = env('DEEPL_SECRET_KEY')
 CHATGPT_SECRET_API = env("CHATGPT_API_KEY")
 openai.api_key = CHATGPT_SECRET_API
 
@@ -36,23 +39,96 @@ openai.api_key = CHATGPT_SECRET_API
 # 適さない表現も存在するのでどのようにする必要がある
 # ウエストについて
 # 体のフィット感についての好み
+# 全てのコーデは網羅しきれない
+
+#作成された画像はs3に保存
+
+season = {
+    1 : 'winter',
+    2 : 'winter',
+    3 : 'spring',
+    4 : 'spring',
+    5 : 'spring',
+    6 : 'summer',
+    7 : 'summer',
+    8 : 'summer',
+    9 : 'autumn',
+    10 : 'autumn',
+    11 : 'autumn',
+    12 : 'winter'
+}
+
+def get_season():
+    now = datetime.datetime.now()
+    month = now.month
+    data = season[int(month)]
+    return data
+
+def translate(content):
+    translator = deepl.Translator(DEEPL_SECRET_KEY)
+    result = translator.translate_text(content, target_lang="EN-US")
+    print(result.text)
+    return result.text
+
 
 class CreateImageView(APIView):
+    permission_classes = (AllowAny,)
+    #生成した画像からどのように洋服をおすすめするか
     def post(self, request):
         data = request.data
-        silhouette = data['silhouette']
+        age = data['age']
+        gender = data['gender']
+        height = data['height']
+        weight = data['weight']
+        size = data['size']
+        silhouette  = data['silhouette']
         brand = data['brand']
         style = data['style']
         color = data['color']
         material = data['material']
         situation = data['situation']
-        #promptを作成
-        prompt = "Desired 170cm 45kg female full body styling, image(Season: Spring, Scene: party,  Age: 20s, style: minimal style, favorite color: black, Tops: oversize tailored jacket, Bottom: Flounce Detail Midi Skirt, Shoes: black ballet shoes, socks:flower-patterned socks, Favorite Brand: simone rocha)"
+        season = get_season()
+        #deepl
+        content = "好きな色は{color}、服のシルエットは{silhouette}、好きなスタイルは{style}、好きなブランドは{brand}、服を着用するシーンは{situation}、好みの素材は{material}、身長は{height}cm、体重は{weight}kg、性別は{gender}、季節は{season}、以下の情報をもとにスタイリングを作成しデザインやディテールまで詳細に記載したアイテムについて他の文章は必要ないので{{tops:"", bottoms:"", inner:"",shoes:"",accessory:""}}のJSON形式だけで返して尚アイテムに関しては情報をもとにtops(シャツ、ポロシャツ、ブラウス、カーディーガン、ニット、チュニック、ワンピース、ジャケット、コートなど）、\
+                   bottoms(デニム、スラックス、スカートなど）例に示したアイテムだけではなく考えられる全てのアイテムから情報に適したアイテムを選択すること。カラーに関しては好きな色の{color}を主体としつつ合うカラーパレットでスタイリングを組むこと。前の返答と同様の形式でjson形式の文章のみ返して".format(
+            color=color,silhouette=silhouette,style=style,brand=brand,situation=situation,material=material, height=height,weight=weight,gender=gender,season=season
+        )
+        
+        #上記の文章を翻訳する
+        result = translate(content)
+
+        completion = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo",
+            messages = [
+                {
+                    "role": "assistant",
+                    "content": 
+                    str({
+                    "tops": "黒いビッグサイズのモードスタイルブラウス、立体的なディテールとレイヤードデザインが特徴",
+                    "bottoms": "ワイドレッグの黒いクロップドパンツ、ストラクチャードなシルエットでモード感を強調",
+                    "inner": "黒いシルク製キャミソール  、通気性が良く軽量な素材",
+                    "shoes": "ブラックのプラットフォームブーツ、モードスタイルをさらに引き立てるデザイン",
+                    "accessory": "黒いアシンメトリックなデザインのイヤリング、モードスタイルに相応しい個性的なアクセント"
+                    })
+                },
+                {
+                    "role": "user",
+                    "content": result
+                }
+            ]
+        )
+        print(completion.choices[0].message)
+        data = json.loads(completion.choices[0].message["content"])
+        print(data)
+        prompt = "Desired {height}cm {weight}kg {gender} full body styling, image(Season: {season}, Scene: {situation},  Age: {age}s, style: {style}, favorite color: {color}, Tops: {tops}, Bottom: {bottoms}, Shoes: {shoes}, accessory:{accessory}, Favorite Brand: {brand})".format(height=height, weight=weight, gender={gender}, season={get_season()}, 
+        situation={situation}, age={age}, brand={brand}, style={style}, color={color}, tops={data['tops']}, bottoms={data['bottoms']}, shoes={data['shoes']},accessory={data['accessory']})
+
+        translator_prmpt = translate(prompt)
 
         #profile情報から身体に関しての情報を取得する    
-        prompt = "Desired 170cm 45kg female full body styling, image(Season: Spring, Scene: party,  Age: 20s, style: minimal style, favorite color: black, Tops: oversize tailored jacket, Bottom: Flounce Detail Midi Skirt, Shoes: black ballet shoes, socks:flower-patterned socks, Favorite Brand: simone rocha)"
+        #   prompt = "Desired 170cm 45kg female full body styling, image(Season: Spring, Scene: party,  Age: 20s, style: minimal style, favorite color: black, Tops: oversize tailored jacket, Bottom: Flounce Detail Midi Skirt, Shoes: black ballet shoes, socks:flower-patterned socks, Favorite Brand: simone rocha)"
         response = openai.Image.create(
-            prompt = prompt,
+            prompt = translator_prmpt,
             n = 1,
             size="1024x1024"
         )
